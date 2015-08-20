a3ee87449f8f5f6c1c3b42522abdebe5454f82f5
Jorge Hernandez <jorge.hernandez@bbts.co>
2015-08-20 10:14:45 -0430

    QuickFix - Script to Fix Splits in DB

---
+++ FixSpDate/ConsoleApplication1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Program;
+
+namespace Program
+{
+    class Program
+    {
+        public class StockSplitHistory
+        {
+            public int SecurityKey;
+            public DateTime SplitDate;
+            public double InverseSplitFactor;
+
+            public StockSplitHistory(int sec_key, DateTime date, double factor)
+            {
+                this.SecurityKey = sec_key;
+                this.SplitDate = date;
+                this.InverseSplitFactor = factor;
+            }
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Read the Residual Data and load into memory filter only the espected profit greater than 0.005
+            string path = "";
+            var dialog = new OpenFileDialog();
+            List<StockSplitHistory> Splits = new List<StockSplitHistory>();
+            using (dialog)
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = dialog.FileName;
+
+                }
+            }
+            // code executed on opened excel file goes here.
+            using (System.IO.StreamReader file = new System.IO.StreamReader(path))
+            {
+                string line = file.ReadLine(); // read header
+                Redshift Redshift_Handler = new Redshift("blackbox.cy7bpvnuirbs.us-east-1.redshift.amazonaws.com", "5439", "masteruser", "Passw0rd!", "blackbox");
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    int SecurityKey = int.Parse(values[0]);
+                    DateTime SplitDate = DateTime.Parse(values[1]);
+                    double oldshares = Convert.ToDouble(values[2]);
+                    double newshares = Convert.ToDouble(values[3]);
+
+                    double InverseSplitFactor = oldshares / newshares;
+                    // new price = oldprice * InverseSplitFactor
+                    StockSplitHistory Split = new StockSplitHistory(SecurityKey, SplitDate, InverseSplitFactor);
+                    Splits.Add(Split);                                       
+
+                }
+                // Sort SlitDates
+                Splits.OrderBy(x => x.SplitDate);
+
+                foreach(StockSplitHistory Record in Splits)
+                {
+                    
+                    // Query to fix Price
+                    string query = "";
+                    // Fundamentals Table
+                    // Last_Split Date
+                    query = string.Format("update fundamentals set last_split = market_date where market_date < '{0}' and sec_key = '{1}';", Record.SplitDate.ToString("yyyy/MM/dd"), Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+                    // Open Price
+                    query = string.Format("update fundamentals set open_price = open_price * {1} where market_date < '{0}' and sec_key = '{2}';", Record.SplitDate.ToString("yyyy / MM / dd"), Record.InverseSplitFactor, Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+                    //previous_close
+                    query = string.Format("update fundamentals set previous_close = previous_close * {1} where market_date < '{0}' and sec_key = '{2}';", Record.SplitDate.ToString("yyyy / MM / dd"), Record.InverseSplitFactor, Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+                    // high price
+                    query = string.Format("update fundamentals set high_price = high_price * {1} where market_date < '{0}' and sec_key = '{2}';", Record.SplitDate.ToString("yyyy / MM / dd"), Record.InverseSplitFactor, Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+                    // low price
+                    query = string.Format("update fundamentals set low_price = low_price * {1} where market_date < '{0}' and sec_key = '{2}';", Record.SplitDate.ToString("yyyy / MM / dd"), Record.InverseSplitFactor, Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+                    // previous_high
+                    query = string.Format("update fundamentals set previous_high = previous_high * {1} where market_date < '{0}' and sec_key = '{2}';", Record.SplitDate.ToString("yyyy / MM / dd"), Record.InverseSplitFactor, Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+                    // previous_low
+                    query = string.Format("update fundamentals set previous_low = previous_low * {1} where market_date < '{0}' and sec_key = '{2}';", Record.SplitDate.ToString("yyyy / MM / dd"), Record.InverseSplitFactor, Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+
+                    // Signals Table 
+                    // last price
+                    query = string.Format("update fundamentals set last_price = last_price * {1} where daytime < '{0}' and sec_key = '{2}';", Record.SplitDate.ToString("yyyy-MM-dd HH:mm:ss"), Record.InverseSplitFactor, Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+                    // bid
+                    query = string.Format("update fundamentals set bid = bid * {1} where daytime < '{0}' and sec_key = '{2}';", Record.SplitDate.ToString("yyyy-MM-dd HH:mm:ss"), Record.InverseSplitFactor, Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+                    // ask
+                    query = string.Format("update fundamentals set ask = ask * {1} where daytime < '{0}' and sec_key = '{2}';", Record.SplitDate.ToString("yyyy-MM-dd HH:mm:ss"), Record.InverseSplitFactor, Record.SecurityKey);
+                    Redshift_Handler.RunQuery(query);
+
+                }
+                
+            }
+        }
+    }
+}
---
+++ FixSpDate/ConsoleApplication1/Redshift.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+
+
+namespace Program
+{
+    /// <summary>
+    //Database=dev; 
+    //UID=masteruser; 
+    //PWD=Passw0rd!; Port=5439
+    /// </summary>
+    /// <param name="_server">Server, e.g. "blackbox.cthwpi11m2oe.us-east-1.redshift.amazonaws.com"</param>
+    /// <param name="_port">Port, e.g. "5439"</param>
+    /// <param name="_masterUsername">MasterUserName, e.g. "masteruser"</param>
+    /// <param name="_masterUserPassword">MasterUserPassword, e.g. "mypassword"</param>
+    /// <param name="_DBName"> DBName, e.g. "devdatabase"</param>
+    public class Redshift : IDisposable
+    {
+
+        OdbcConnection conn;
+
+        // Server, e.g. "examplecluster.xyz.us-east-1.redshift.amazonaws.com"
+        string server = "";
+        // Port, e.g. "5439"
+        string port = "";
+        // MasterUserName, e.g. "masteruser".
+        string masterUsername = "";
+        // MasterUserPassword, e.g. "mypassword".
+        string masterUserPassword = "";
+        // DBName, e.g. "devdatabase"
+        string DBName = "";
+        string query = "";
+        /// <summary>
+        //Database=dev; 
+        //UID=masteruser; 
+        //PWD=Passw0rd!; Port=5439
+        /// </summary>
+        /// <param name="_server">Server, e.g. "blackbox.cthwpi11m2oe.us-east-1.redshift.amazonaws.com"</param>
+        /// <param name="_port">Port, e.g. "5439"</param>
+        /// <param name="_masterUsername">MasterUserName, e.g. "masteruser"</param>
+        /// <param name="_masterUserPassword">MasterUserPassword, e.g. "mypassword"</param>
+        /// <param name="_DBName"> DBName, e.g. "devdatabase"</param>
+        public Redshift(string _server, string _port, string _masterUsername, string _masterUserPassword, string _DBName)
+        {
+            server = _server;
+            port = _port;
+            masterUsername = _masterUsername;
+            masterUserPassword = _masterUserPassword;
+            DBName = _DBName;
+        }
+
+        public void EnsureConnectionIsOpen()
+        {
+
+            if (conn == null)
+            {
+
+                // Create the ODBC connection string.
+                string connString = "Driver={PostgreSQL Unicode};" +
+            string.Format("Server={0};Database={1};" +
+            "UID={2};PWD={3};Port={4};SSL=true;Sslmode=Require",
+                    server, DBName, masterUsername,
+                    masterUserPassword, port);
+
+                // Make a connection using the psqlODBC provider.
+                conn = new OdbcConnection(connString);
+                conn.Open();
+
+            }
+
+        }
+
+        public List<object[]> RunQuery(string _query, ref string Error)
+        {
+
+            DateTime init_time = DateTime.Now;
+            // Create the ODBC connection string.  
+            string connString = "Driver={PostgreSQL Unicode};" + string.Format("Server={0};Database={1};" + "UID={2};PWD={3};Port={4};SSL=true;Sslmode=Require", server, DBName, masterUsername, masterUserPassword, port);
+            OdbcDataReader reader;
+            List<object[]> Results = new List<object[]>();
+            try
+            {
+
+                using (OdbcConnection connection = new OdbcConnection(connString))
+                {
+                    OdbcCommand myCommand = new OdbcCommand(_query, connection);
+
+                    connection.Open();
+                    reader = myCommand.ExecuteReader();
+                    // Now we have an OdbcDataReader, we can find the number and names of the columns in the result set, and display this on the console.
+                    Error = "Query executed succesfully" + Environment.NewLine;
+                    int fCount = reader.FieldCount;
+                    string[] resultquery = new string[fCount];
+                    for (int i = 0; i < fCount; i++)
+                    {
+                        string fName = reader.GetName(i);
+                        resultquery[i] = (fName);
+                    }
+                    string[] queryrow = new string[fCount];
+                    resultquery.CopyTo(queryrow, 0);
+                    Results.Add(queryrow);
+
+                    // Now we can read each row from the result set, and read each column in that row and display its value.
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < fCount; i++)
+                        {
+                            string col = (reader.IsDBNull(i)) ? "NULL" : (reader.GetValue(i).ToString());
+
+                            resultquery[i] = (col);
+                        }
+                        string[] queryrow1 = new string[fCount];
+
+                        resultquery.CopyTo(queryrow1, 0);
+                        Results.Add(queryrow1);
+                    }
+                }
+            }
+            catch (Exception Query)
+            {
+                Exception FailedQuery = new Exception(string.Format("{0} failed query : {1}", Query.Message, _query));
+                // Report.Error(710, FailedQuery);
+                Error = "An error occurred when executing the SQL command: " + _query + Environment.NewLine + Query.Message + Environment.NewLine;
+            }
+            Error += string.Format("Execution time: {0}", (DateTime.Now - init_time).TotalSeconds.ToString("f2"));
+
+            return Results;
+        }
+        public List<object> RunQuery(string _query)
+        {
+
+            // Create the ODBC connection string.  
+            string connString = "Driver={PostgreSQL Unicode};" + string.Format("Server={0};Database={1};" + "UID={2};PWD={3};Port={4};SSL=true;Sslmode=Require", server, DBName, masterUsername, masterUserPassword, port);
+            OdbcDataReader reader;
+
+            List<object> Results = new List<object>();
+
+            using (OdbcConnection connection = new OdbcConnection(connString))
+            {
+                OdbcCommand myCommand = new OdbcCommand(_query, connection);
+
+                connection.Open();
+                reader = myCommand.ExecuteReader();
+                bool all_results = true;
+                do
+                {
+                    object[] query_result = new object[25];
+                    all_results = reader.Read();
+                    if (all_results)
+                    {
+                        reader.GetValues(query_result);
+                    }
+                    // save the query_result to the list
+                    Results.AddRange(query_result.Select(x => x).Where(q => q != null).ToList());
+
+
+                    if (!all_results)
+                    {
+                        // get next result
+                        all_results = reader.NextResult();
+                    }
+                } while (all_results); // loop throgh all responses 
+
+            }
+
+
+
+            return Results;
+        }
+
+        public List<string> RunCountQuery(string _query)
+        {
+            string connString = "Driver={PostgreSQL Unicode};" + string.Format("Server={0};Database={1};" + "UID={2};PWD={3};Port={4};SSL=true;Sslmode=Require", server, DBName, masterUsername, masterUserPassword, port);
+            OdbcDataReader reader;
+
+            List<string> Results = new List<string>();
+
+            using (OdbcConnection connection = new OdbcConnection(connString))
+            {
+                OdbcCommand myCommand = new OdbcCommand(_query, connection);
+
+                connection.Open();
+                reader = myCommand.ExecuteReader();
+                bool all_results = true;
+                all_results = reader.Read();
+                while (all_results)
+                {            // loop throgh all responses              
+
+                    // save the query_result to the list
+                    Results.Add(reader[0].ToString());
+                    all_results = reader.Read();
+                    if (!all_results)
+                    {
+                        // get next result
+                        all_results = reader.NextResult();
+                        if (all_results) reader.Read();
+                    }
+                }
+            }
+
+
+            return Results;
+        }
+
+
+        public void Dispose()
+        {
+            if (conn != null)
+                conn.Close();
+
+        }
+    }
+}
